1008b9db64edd54559b33753d883ba1a1267f20d
Jacob Morse <jmorse1111@gmail.com>
2016-05-20 11:24:43 -0500

    Morse_3_SlickGame

---
+++ src/arrays/Treasure.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author jacobiscoolio
+ */
+public class Treasure {
+    int x;
+    int y;
+    int score = 1; 
+
+    Treasure(int a, int b) {
+        this.x = a;
+        this.y = b;
+       
+    }
+}
